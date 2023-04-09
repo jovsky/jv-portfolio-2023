8239862e2c3367a55c76d38637cddccc41b808b9
Joao <jvmelop@gmail.com>
2023-04-09 20:53:19 -0300

    exibindo card

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/">Projetos</router-link> |
+        <router-link to="/about">Sobre</router-link>
     </nav>
     <router-view />
 </template>
 
 <style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
+body {
+    background-image: url("https://img.wallpapersafari.com/desktop/1366/768/50/12/YSpoRh.jpg");
+    background-color: rgb(82, 82, 82);
+    overflow: hidden;
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
         color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        height: 95vh;
+        align-items: center;
+        nav {
+            padding: 30px;
+            a {
+                font-weight: bold;
+                text-decoration: none;
+                color: #ffffff;
+                &.router-link-exact-active {
+                    color: #c9ffe7;
+                }
+            }
         }
     }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,95 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="portfolio">
+        <div class="portfolio__background"></div>
+        <div class="portfolio__shadow"></div>
+        <span class="portfolio__title">PROJECTS</span>
+        <div class="portfolio__list">
+            <div
+                class="portfolio__list__item"
+                v-for="data in projectsData"
+                :key="data.name"
+            >
+                <ProjectCard :data="data" />
+            </div>
+        </div>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-    name: "HomeView",
-    components: {
-        HelloWorld,
-    },
-});
+<script lang="ts" setup>
+import ProjectCard from "@/components/ProjectCard.vue"; // @ is an alias to /src
+import projectsData from "@/data/projects";
 </script>
+
+<style lang="scss" scoped>
+.portfolio {
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 50px;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border: 5px dashed black;
+    border-style: dashed;
+    box-sizing: content-box;
+    border-radius: 10px;
+    overflow: hidden;
+    padding: 0 40px;
+
+    &__title {
+        color: black;
+        margin: 30px;
+        font-size: 20px;
+        color: black;
+        font-weight: 800;
+        font-style: black;
+        font-family: "Courier New", Courier, monospace;
+        letter-spacing: 3px;
+        text-shadow: 3px 3px 10px rgb(0, 255, 200);
+    }
+    &__background {
+        z-index: -1;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.8);
+        backdrop-filter: blur(10px);
+    }
+    &__shadow {
+        position: absolute;
+        z-index: 10;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        box-shadow: inset 0px 0px 10px 10px rgba(0, 0, 0, 0.411);
+        pointer-events: none;
+    }
+    &__list {
+        display: flex;
+        width: 100%;
+        gap: 20px;
+        height: 100%;
+        justify-content: flex-start;
+        align-items: center;
+        flex-direction: column;
+        overflow-y: auto;
+        overflow-x: hidden;
+        padding: 40px;
+        &__item {
+            width: 100%;
+            display: flex;
+        }
+    }
+}
+// @media screen and (max-width: 1060px) {
+//     .portfolio {
+//         width: 100%;
+//         margin: 0 30px;
+//     }
+// }
+</style>
